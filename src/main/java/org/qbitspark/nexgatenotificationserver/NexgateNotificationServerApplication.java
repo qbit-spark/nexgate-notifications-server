e227077fc6ae1b67feda8b1b5fa4f475d7f99c09
frontpage656 <joshuasimon656@gmail.com>
2025-10-18 23:50:06 +0300

    enable textfy from prop file

--- src/main/java/org/qbitspark/nexgatenotificationserver/NexgateNotificationServerApplication.java
+++ src/main/java/org/qbitspark/nexgatenotificationserver/NexgateNotificationServerApplication.java
@@ -9,5 +9,4 @@ public class NexgateNotificationServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(NexgateNotificationServerApplication.class, args);
     }
-
 }
